4bd8f65ad7fd76ac66d200962e8803c2a0a845fe
Pasoon Azimi <pasoon.azimi10@gmail.com>
2018-11-27 18:11:42 -0500

    project structure

---
+++ src/containers/Login.js
@@ -0,0 +1,17 @@
+
+import styled from 'styled-components';
+
+class Login extends Component {
+
+
+	render() {
+
+
+		return (
+			// React-big-calendar requires container to have explicit height
+            <div></div>
+		);
+	}
+}
+
+export default Calendar;
