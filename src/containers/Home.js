0acd851c0731f5a4d152dd950765684fbcc2970d
Unaiz Rehmani <unaizrehmani@gmail.com>
2018-11-28 19:58:23 -0500

    add Home.js component

---
+++ src/containers/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render = () => {
+    return <p>In Home Component</p>;
+  };
+}
+
+export default Home;
