cc3d888d130f55b4b6b80cb141ace5a94fa4443e
Unaiz Rehmani <unaizrehmani@gmail.com>
2018-12-02 18:09:55 -0500

    fix and test propertiesList.js

--- src/containers/PropertiesList.js
+++ src/containers/PropertiesList.js
@@ -1,23 +1,28 @@
 import React, { Component } from "react";
 import SingleProperty from "../components/SingleProperty";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 class PropertiesList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   renderProperties = () => {
-    let properties = this.props.properties || [];
-    let jsxProperties = [];
-    properties.forEach((property, index) => {
-      jsxProperties.push(<SingleProperty key={index} property={property} />);
-    });
-    return jsxProperties;
+    if (this.props.properties) {
+      let jsxProperties = [];
+      this.props.properties.forEach((property, index) => {
+        jsxProperties.push(<SingleProperty key={index} property={property} />);
+      });
+      return jsxProperties;
+    } else {
+      return <div>You don't own any properties</div>;
+    }
   };
+
   render = () => {
     return <div className="container">{this.renderProperties()}</div>;
   };
 }
-
-PropertiesList.propTypes = {
-  properties: PropTypes.array.isRequired
-};
 
 export default PropertiesList;
