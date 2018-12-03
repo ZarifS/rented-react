d77265be7bd12ed253fdc87bb77623fe88bf190c
Unaiz Rehmani <unaizrehmani@gmail.com>
2018-12-03 01:05:56 -0500

    remove warning signs

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,17 @@
-import React, {Component} from 'react';
-import {FormControl, FormGroup } from 'react-bootstrap';
-import SearchBar from 'material-ui-search-bar';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import styled from 'styled-components';
-import AutoComplete from 'material-ui/AutoComplete';
-import Select from 'react-select';
+import React, { Component } from "react";
+import { FormControl, FormGroup } from "react-bootstrap";
+import SearchBar from "material-ui-search-bar";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import styled from "styled-components";
+import AutoComplete from "material-ui/AutoComplete";
+import Select from "react-select";
 
 const HorizontalWrapper = styled.div`
   display: flex;
   flex-direction: row;
   margin: 15px auto;
   max-width: 800px;
-`
+`;
 
 const FilterByText = styled.span`
   margin-right: 5px;
@@ -29,100 +29,125 @@ const CustomSelect = styled(Select)`
   width: 120px;
 `;
 
-
 const typeOptions = [
-  { value: '', label: 'All' },
-  { value: 'apartment', label: 'Apartment' },
-  { value: 'condo', label: 'Condo' },
-  { value: 'house', label: 'House' },
-]
+  { value: "", label: "All" },
+  { value: "apartment", label: "Apartment" },
+  { value: "condo", label: "Condo" },
+  { value: "house", label: "House" }
+];
 
 const locationOptions = [
-  { value: '', label: 'All' },
-  { value: 'ottawa', label: 'Ottawa' },
-  { value: 'gatineau', label: 'Gatineau' },
-  { value: 'toronto', label: 'Toronto' },
-]
+  { value: "", label: "All" },
+  { value: "ottawa", label: "Ottawa" },
+  { value: "gatineau", label: "Gatineau" },
+  { value: "toronto", label: "Toronto" }
+];
 
-
- class Search extends Component {
-    constructor(props) {
-      super(props)
-    }
-
-    handleChange = (e) => {
-      this.props.callbackSV(e);
-    }
-
-    handleBedrooms = (e) => {
-      if(e.target.value == ""){
-        this.props.callbackBed(0);
-      }
-      else{
-        this.props.callbackBed(e.target.value);
-      }
-    }
-
-    handleBathrooms = (e) => {
-      if(e.target.value == ""){
-        this.props.callbackBath(0);
-      }
-      else{
-        this.props.callbackBath(e.target.value);
-      }
-    }
-
-    handleLocation = (e) => {
-      this.props.callbackLocation(e.value);
-    }
-    
-    handleType = (e) => {
-      this.props.callbackType(e.value);
-    }
-
-    handleMinRent = (e) => {
-      if(e.target.value == ""){
-        this.props.callbackMinRent(0);
-      }
-      else{
-        this.props.callbackMinRent(e.target.value);
-      }
-    }
-
-    handleMaxRent = (e) => {
-      if(e.target.value == ""){
-        this.props.callbackMaxRent(0);
-      }
-      else{
-        this.props.callbackMaxRent(e.target.value);
-      }
-    }
-
-    render() {
-      return (
-      <div>
-        <MuiThemeProvider>
-        <SearchBar
-          onChange={this.handleChange}
-          style={{
-            margin: '15px auto',
-            maxWidth: 800
-          }}
-          hintText={"Search for a Property"}
-        />
-        <HorizontalWrapper>
-          <FilterByText>Filter By: </FilterByText>
-          <Input type="number" name="bedrooms" placeholder="Bedrooms" onChange={this.handleBedrooms}></Input>
-          <Input type="number" name="bathrooms" placeholder="Bathrooms" onChange={this.handleBathrooms}></Input>
-          <CustomSelect placeholder={"Location"} options={locationOptions} onChange={this.handleLocation}/>
-          <CustomSelect placeholder={"Type"} options={typeOptions} onChange={this.handleType}/>
-          <Input type="number" name="min rent" placeholder="Min Rent" onChange={this.handleMinRent}></Input>
-          <Input type="number" name="max rent" placeholder="Max Rent" onChange={this.handleMaxRent}></Input>
-        </HorizontalWrapper>
-        </MuiThemeProvider>
-      </div>
-      );
-    }
+class Search extends Component {
+  constructor(props) {
+    super(props);
   }
 
-  export default Search;
+  handleChange = e => {
+    this.props.callbackSV(e);
+  };
+
+  handleBedrooms = e => {
+    if (e.target.value == "") {
+      this.props.callbackBed(0);
+    } else {
+      this.props.callbackBed(e.target.value);
+    }
+  };
+
+  handleBathrooms = e => {
+    if (e.target.value == "") {
+      this.props.callbackBath(0);
+    } else {
+      this.props.callbackBath(e.target.value);
+    }
+  };
+
+  handleLocation = e => {
+    this.props.callbackLocation(e.value);
+  };
+
+  handleType = e => {
+    this.props.callbackType(e.value);
+  };
+
+  handleMinRent = e => {
+    if (e.target.value == "") {
+      this.props.callbackMinRent(0);
+    } else {
+      this.props.callbackMinRent(e.target.value);
+    }
+  };
+
+  handleMaxRent = e => {
+    if (e.target.value == "") {
+      this.props.callbackMaxRent(0);
+    } else {
+      this.props.callbackMaxRent(e.target.value);
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <div>
+            <SearchBar
+              onChange={this.handleChange}
+              style={{
+                margin: "15px auto",
+                maxWidth: 800
+              }}
+              onRequestSearch={() => {}}
+              hintText={"Search for a Property"}
+            />
+            <HorizontalWrapper>
+              <FilterByText>Filter By: </FilterByText>
+              <Input
+                type="number"
+                name="bedrooms"
+                placeholder="Bedrooms"
+                onChange={this.handleBedrooms}
+              />
+              <Input
+                type="number"
+                name="bathrooms"
+                placeholder="Bathrooms"
+                onChange={this.handleBathrooms}
+              />
+              <CustomSelect
+                placeholder={"Location"}
+                options={locationOptions}
+                onChange={this.handleLocation}
+              />
+              <CustomSelect
+                placeholder={"Type"}
+                options={typeOptions}
+                onChange={this.handleType}
+              />
+              <Input
+                type="number"
+                name="min rent"
+                placeholder="Min Rent"
+                onChange={this.handleMinRent}
+              />
+              <Input
+                type="number"
+                name="max rent"
+                placeholder="Max Rent"
+                onChange={this.handleMaxRent}
+              />
+            </HorizontalWrapper>
+          </div>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+
+export default Search;
--- src/containers/BrowseProperties.js
+++ src/containers/BrowseProperties.js
@@ -1,25 +1,23 @@
 import styled from "styled-components";
 import React, { Component } from "react";
-import Search from '../components/Search';
-import Grid from '@material-ui/core/Grid';
-import { withStyles } from '@material-ui/core/styles';
-import CustomPaper from '../components/CustomPaper';
-import SingleProperty from '../components/SingleProperty';
+import Search from "../components/Search";
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
+import CustomPaper from "../components/CustomPaper";
+import SingleProperty from "../components/SingleProperty";
 
 const styles = theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      height: 300,
-      width: 250,
-    },
-    control: {
-      padding: theme.spacing.unit * 2,
-    },
-  });
-
-
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    height: 300,
+    width: 250
+  },
+  control: {
+    padding: theme.spacing.unit * 2
+  }
+});
 
 class BrowseProperties extends Component {
   constructor(props) {
@@ -33,77 +31,94 @@ class BrowseProperties extends Component {
       locationFilter: "",
       typeFilter: "",
       minRentFilter: 0,
-      maxRentFilter: 10000000,
+      maxRentFilter: 10000000
     };
 
+    this.searchValueCallBack = dataFromChild => {
+      this.setState({ searchValue: dataFromChild });
+    };
 
-    this.searchValueCallBack = (dataFromChild) => {
-        this.setState({searchValue: dataFromChild});
-    }
+    this.bathRoomCallBack = dataFromChild => {
+      this.setState({ bathRoomFilter: dataFromChild });
+    };
 
-    this.bathRoomCallBack = (dataFromChild) => {
-      this.setState({bathRoomFilter: dataFromChild})
-    }
+    this.bedRoomCallBack = dataFromChild => {
+      this.setState({ bedRoomFilter: dataFromChild });
+    };
 
-    this.bedRoomCallBack = (dataFromChild) => {
-      this.setState({bedRoomFilter: dataFromChild})
-    }
+    this.locationCallBack = dataFromChild => {
+      this.setState({ locationFilter: dataFromChild });
+    };
 
-    this.locationCallBack = (dataFromChild) => {
-      this.setState({locationFilter: dataFromChild})
-    }
+    this.typeCallBack = dataFromChild => {
+      this.setState({ typeFilter: dataFromChild });
+    };
 
-    this.typeCallBack = (dataFromChild) => {
-      this.setState({typeFilter: dataFromChild})
-    }
+    this.minRentCallBack = dataFromChild => {
+      this.setState({ minRentFilter: dataFromChild });
+    };
 
-    this.minRentCallBack = (dataFromChild) => {
-      this.setState({minRentFilter: dataFromChild})
-    }
-
-    this.maxRentCallBack = (dataFromChild) => {
-      this.setState({maxRentFilter: dataFromChild})
-    }
-
-
+    this.maxRentCallBack = dataFromChild => {
+      this.setState({ maxRentFilter: dataFromChild });
+    };
   }
 
   render() {
-
     const { classes } = this.props;
 
-    if(!this.props.properties || this.props.properties.length < 1){
-      return(<div/>)
+    if (!this.props.properties || this.props.properties.length < 1) {
+      return <div />;
     }
 
-    let filterByName = this.props.properties.filter(property => property.title.toLowerCase().includes(this.state.searchValue.toLowerCase()));
-    if(this.state.searchValue == "" || this.state.searchValue.length < 1){
+    let filterByName = this.props.properties.filter(property =>
+      property.title
+        .toLowerCase()
+        .includes(this.state.searchValue.toLowerCase())
+    );
+    if (this.state.searchValue == "" || this.state.searchValue.length < 1) {
       filterByName = this.props.properties;
     }
-    let filterByBedroom = filterByName.filter(property => property.bedrooms >= parseInt(this.state.bedRoomFilter));
-    let filterByBathroom = filterByBedroom.filter(property => property.bathrooms >= parseInt(this.state.bathRoomFilter));
-    let filterByMinRent = filterByBathroom.filter(property => property.rent >= parseInt(this.state.minRentFilter));
-    let filterByMaxRent = filterByMinRent.filter(property => property.rent <= parseInt(this.state.maxRentFilter));
-    let filterByLocation = filterByMaxRent.filter(property => property.city.toLowerCase().includes(this.state.locationFilter));
-    let filteredproperties = filterByLocation.filter(property => property.type.toLowerCase().includes(this.state.typeFilter));
-    
+    let filterByBedroom = filterByName.filter(
+      property => property.bedrooms >= parseInt(this.state.bedRoomFilter)
+    );
+    let filterByBathroom = filterByBedroom.filter(
+      property => property.bathrooms >= parseInt(this.state.bathRoomFilter)
+    );
+    let filterByMinRent = filterByBathroom.filter(
+      property => property.rent >= parseInt(this.state.minRentFilter)
+    );
+    let filterByMaxRent = filterByMinRent.filter(
+      property => property.rent <= parseInt(this.state.maxRentFilter)
+    );
+    let filterByLocation = filterByMaxRent.filter(property =>
+      property.city.toLowerCase().includes(this.state.locationFilter)
+    );
+    let filteredproperties = filterByLocation.filter(property =>
+      property.type.toLowerCase().includes(this.state.typeFilter)
+    );
 
     return (
       <div className={classes.root}>
-      <Grid container spacing={24}>
-            <Grid item xs={12}>
-                <Search callbackSV={this.searchValueCallBack} callbackBed={this.bedRoomCallBack} callbackBath={this.bathRoomCallBack} 
-                callbackLocation={this.locationCallBack} callbackType={this.typeCallBack} 
-                callbackMinRent={this.minRentCallBack} callbackMaxRent={this.maxRentCallBack}/>
-            </Grid>
-            <Grid item xs={12}>
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <Search
+              callbackSV={this.searchValueCallBack}
+              callbackBed={this.bedRoomCallBack}
+              callbackBath={this.bathRoomCallBack}
+              callbackLocation={this.locationCallBack}
+              callbackType={this.typeCallBack}
+              callbackMinRent={this.minRentCallBack}
+              callbackMaxRent={this.maxRentCallBack}
+            />
+          </Grid>
+          <Grid item xs={12}>
             <h3>Browse </h3>
-        </Grid>
-            {filteredproperties.map((property, index) => (
-              <Grid key={property} item xs={6}>
-                <SingleProperty property={property}/>
-              </Grid>
-            ))}
+          </Grid>
+          {filteredproperties.map((property, index) => (
+            <Grid key={index} item xs={6}>
+              <SingleProperty property={property} />
+            </Grid>
+          ))}
         </Grid>
       </div>
     );
